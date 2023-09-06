ea6fb614ed522014f2fdf7c96eec4c0a92475ca0
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2023-09-06 16:43:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function preload() {
+x = 0;
+y = 0;
+score = 0;
 
-}
+
+function preload() {}
 
 function setup() {
     createCanvas(1280,720);
@@ -13,7 +16,20 @@ function setup() {
 }
 
 function draw() {
+    if(score > 0.2) {
+        fill('#454545');
+        stroke('#606060');
+        circle(x, y, 40);
+    }
     image(img, 350, 350, 1280, 720);
 }
 
 function modelLoaded() {console.log('model loaded');}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        x = ml5.pose.rightwrist.x;
+        y = ml5.pose.rightwrist.y;
+        score = ml5.pose.rightwrist.score;
+    } 
+}
